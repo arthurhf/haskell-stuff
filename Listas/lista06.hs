15883f10281e9205ba072c482ad1e3a02f6a8455
Edu Renesto <edure95@gmail.com>
2019-08-27 18:12:00 -0300

    Resolve parcialmente exercicio 4

--- Listas/lista06.hs
+++ Listas/lista06.hs
@@ -3,7 +3,7 @@
 data Tree a = Leaf | Node  (Tree a) a (Tree a) deriving Show
 
 instance Functor Tree where
-  fmap :: (a -> b) -> Tree a -> Tree b
+  --fmap :: (a -> b) -> Tree a -> Tree b
   -- folha -> retornamos folha novamente
   fmap _ Leaf = Leaf
   -- nó -> retornamos um outro nó com f aplicada nos ramos da esquerda
@@ -15,31 +15,31 @@ instance Functor Tree where
 newtype ZipList a = Z [a] deriving Show
 
 instance Functor ZipList where
-    fmap :: (a -> b) -> ZipList a -> ZipList b
+    --fmap :: (a -> b) -> ZipList a -> ZipList b
     fmap g (Z xs) = Z $ map g xs
 
 -- Dica: https://en.wikibooks.org/wiki/Haskell/Applicative_functors
 -- dá pra entender muito mais fácil
 instance Applicative ZipList where
-    pure :: a -> ZipList a
+    --pure :: a -> ZipList a
     pure x = Z $ repeat x
 
-    (<*>) :: Z (a -> b) -> Z a -> Z b
-    as <*> bs = zipWith ($) as bs
+    --(<*>) :: ZipList (a -> b) -> ZipList a -> ZipList b
+    (Z as) <*> (Z bs) = Z $ zipWith ($) as bs
 
 --ex3
 data Expr a = Var a | Val Int | Add (Expr a) (Expr a) deriving Show
 
 instance Functor Expr where
-    fmap :: (a -> b) -> Expr a -> Expr b
+    --fmap :: (a -> b) -> Expr a -> Expr b
     fmap = undefined
 
 instance Applicative Expr where 
-    pure :: a -> Expr a
+    --pure :: a -> Expr a
     pure = undefined
 
 instance Monad Expr where
-    (>>=) :: Expr a -> (a -> Expr b) -> Expr b
+    --(>>=) :: Expr a -> (a -> Expr b) -> Expr b
     (>>=) = undefined 
 
 
@@ -48,21 +48,30 @@ instance Monad Expr where
 newtype Identity a = Identity a
 
 instance Functor Identity where
-    fmap = undefined
+  fmap f (Identity a) = Identity (f a)
 
 instance Applicative Identity where
-    pure = undefined
+  pure = Identity
+  (Identity f) <*> (Identity a) = Identity (f a)
 
 instance Monad Identity where
-    (>>=) = undefined
+  (Identity a) >>= f = f a
 
 data Pair a = Pair a a
 
 instance Functor Pair where
-    fmap = undefined
+  fmap f (Pair a b) = Pair (f a) (f b)
 
 instance Applicative Pair where
-    pure = undefined
+  pure a = Pair a a
+  (Pair f g) <*> (Pair a b) = Pair (f a) (g b)
+
+-- Deve existir uma melhor maneira pra isso!
+primeiro :: Pair a -> a
+primeiro (Pair a _) = a
+
+segundo :: Pair a -> a
+segundo (Pair _ a) = a
 
 instance Monad Pair where
-    (>>=) = undefined
+  (>>=) = undefined -- Explode, porque iria fazer igual o prod. cartesiano da lista!
